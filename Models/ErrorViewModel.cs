d1ce42a9f65d59395951548c50b3cec2a9040d8f
mauri-rivera <mauri.rivera@outlook.com>
2023-10-27 18:02:37 -0300

    update3

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Rivera_Mauricio_ASPCore_EDojVa.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,26 @@
+#pragma warning disable CS8618
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rivera_Mauricio_ASPCore_EDojVa.Models
+{
+    public class Usuario
+    {
+        [Required(ErrorMessage = "Debe proporcionar un dato!!")]
+        [MinLength(2, ErrorMessage = "Debe tener al menos 2 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Debe proporcionar un dato!!")]
+        public string Ubicacion { get; set; }
+
+        [Required(ErrorMessage = "Debe proporcionar un dato!!")]
+        public string Lenguaje { get; set; }
+
+        [MinLength(21, ErrorMessage = "Debe tener al menos 21 caracteres")]
+        public string? Comentario { get; set; }
+    }
+}
